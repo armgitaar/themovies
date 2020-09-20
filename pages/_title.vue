26e58c0afe9c459c273ee8d2d8e2fb84f5d2fdf1
Adam Miedema <armgitaar@yahoo.com>
2020-09-20 11:30:28 -0600

    part 4 - update front-end to hook up with API

--- pages/_title.vue
+++ pages/_title.vue
@@ -3,14 +3,14 @@
     <header>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">
-          Children of Men
+          {{ movie.title }}
         </h1>
       </div>
     </header>
     <main>
       <div class="flex max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
         <div>
-          <img class="w-full" src="https://m.media-amazon.com/images/M/MV5BMTQ5NTI2NTI4NF5BMl5BanBnXkFtZTcwNjk2NDA2OQ@@._V1_.jpg" alt="">
+          <img class="w-full" :src="movie.poster_image" alt="">
         </div>
         <div class="px-12 max-w-5xl">
           <div>
@@ -20,7 +20,7 @@
                   Released
                 </dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                  2006
+                  {{ movie.release_year }}
                 </dd>
               </div>
               <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
@@ -28,7 +28,7 @@
                   Genres
                 </dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                  Adventure, Drama, Sci-Fi
+                  {{ movie.genres }}
                 </dd>
               </div>
               <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
@@ -36,7 +36,7 @@
                   Top Billed
                 </dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                  Clive Owen, Julianne Moore, Michael Cane
+                  {{ movie.top_billed }}
                 </dd>
               </div>
               <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
@@ -44,7 +44,7 @@
                   Plot
                 </dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                  In 2027, in a chaotic world in which women have become somehow infertile, a former activist agrees to help transport a miraculously pregnant woman to a sanctuary at sea.
+                  {{ movie.movie_description }}
                 </dd>
               </div>
               <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
@@ -52,21 +52,28 @@
                   Review
                 </dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                  The movie is pretty good. It's known for long, seemingly un-cut sequences. It's better than Gravity. It's not as good as 7 Angry Men. You should watch it. I mean, what else are you doing?
+                  {{ movie.movie_review }}
                 </dd>
               </div>
             </dl>
           </div>
         </div>
-
-
       </div>
     </main>
   </div>
 </template>
 
 <script>
-  export default {}
+  import axios from 'axios'
+
+  export default {
+    async asyncData({ params }) {
+      const { data } = await axios.get(`http://0.0.0.0:3333/movies/${params.title}`)
+      return {
+        movie: data
+      }
+    }
+  }
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,30 +9,29 @@
     </header>
     <main>
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
-
         <div class="bg-white shadow overflow-hidden sm:rounded-md">
           <ul>
-            <li>
-              <nuxt-link to="/children_of_men" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
+            <li v-for="movie in movies">
+              <nuxt-link :to="`/${movie.id}`" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
                 <div class="flex items-center px-4 py-4 sm:px-6">
                   <div class="min-w-0 flex-1 flex items-center">
                     <div class="flex-shrink-0">
-                      <img class="h-12" src="https://m.media-amazon.com/images/M/MV5BMTQ5NTI2NTI4NF5BMl5BanBnXkFtZTcwNjk2NDA2OQ@@._V1_.jpg" alt="">
+                      <img class="h-12" :src="movie.poster_image" alt="">
                     </div>
                     <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                       <div>
-                        <div class="text-sm leading-5 font-medium text-indigo-600 truncate">Children of Men</div>
+                        <div class="text-sm leading-5 font-medium text-indigo-600 truncate">{{ movie.title }}</div>
                         <div class="mt-2 flex items-center text-sm leading-5 text-gray-700">
-                          2006 | Adventure, Drama, Sci-Fi
+                          {{ movie.release_year }} | {{ movie.genres }}
                         </div>
                       </div>
                       <div class="hidden md:block">
                         <div>
                           <div class="text-sm leading-5 text-gray-900">
-                            Clive Owen, Julianne Moore, Michael Cane
+                            {{ movie.top_billed }}
                           </div>
                           <div class="mt-2 flex items-center text-sm leading-5 text-gray-600 truncate ...">
-                            In 2027, in a chaotic world in which women have become somehow infertile, a former activist agrees to help transport a miraculously pregnant woman to a sanctuary at sea.
+                            {{ movie.movie_description }}
                           </div>
                         </div>
                       </div>
@@ -46,84 +45,23 @@
                 </div>
               </nuxt-link>
             </li>
-            <li class="border-t border-gray-200">
-              <a href="#" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
-                <div class="flex items-center px-4 py-4 sm:px-6">
-                  <div class="min-w-0 flex-1 flex items-center">
-                    <div class="flex-shrink-0">
-                      <img class="h-12" src="https://m.media-amazon.com/images/M/MV5BYTViNzMxZjEtZGEwNy00MDNiLWIzNGQtZDY2MjQ1OWViZjFmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,666,1000_AL_.jpg" alt="">
-                    </div>
-                    <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                      <div>
-                        <div class="text-sm leading-5 font-medium text-indigo-600 truncate">The Terminator</div>
-                        <div class="mt-2 flex items-center text-sm leading-5 text-gray-700">
-                          1984 | Action, Sci-Fi
-                        </div>
-                      </div>
-                      <div class="hidden md:block">
-                        <div>
-                          <div class="text-sm leading-5 text-gray-900">
-                            Arnold Schwarzenegger, Michael Biehn, Linda Hamilton
-                          </div>
-                          <div class="mt-2 flex items-center text-sm leading-5 text-gray-600 truncate ...">
-                            A human soldier is sent from 2029 to 1984 to stop an almost indestructible cyborg killing machine, sent from the same year, which has been programmed to execute a young woman whose unborn son is the key to humanity's future salvation.                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                  </div>
-                </div>
-              </a>
-            </li>
-            <li class="border-t border-gray-200">
-              <a href="#" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
-                <div class="flex items-center px-4 py-4 sm:px-6">
-                  <div class="min-w-0 flex-1 flex items-center">
-                    <div class="flex-shrink-0">
-                      <img class="h-12" src="https://m.media-amazon.com/images/M/MV5BNmRjYWE3OTQtYzEwOC00OWM4LTk3MzktZTUyZTgzNjY4NDc0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="">
-                    </div>
-                    <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                      <div>
-                        <div class="text-sm leading-5 font-medium text-indigo-600 truncate">Sisterhood of the Traveling Pants</div>
-                        <div class="mt-2 flex items-center text-sm leading-5 text-gray-700">
-                          2005 | Comedy, Drama, Romance
-                        </div>
-                      </div>
-                      <div class="hidden md:block">
-                        <div class="hidden md:block">
-                          <div>
-                            <div class="text-sm leading-5 text-gray-900">
-                              Amber Tamblyn, Alexis Bledel, America Ferrera, Blake Lively
-                            </div>
-                            <div class="mt-2 flex items-center text-sm leading-5 text-gray-600 truncate ...">
-                              Four best girlfriends hatch a plan to stay connected with one another as their lives start off in different directions: they pass around a pair of secondhand jeans that fits each of their bodies perfectly.                          </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                  </div>
-                </div>
-              </a>
-            </li>
           </ul>
         </div>
-
       </div>
     </main>
   </div>
 </template>
 
 <script>
-  export default {}
+  import axios from 'axios'
+  export default {
+    async asyncData() {
+      const { data } = await axios.get('http://0.0.0.0:3333/movies')
+      return {
+        movies: data
+      }
+    }
+  }
 </script>
 
 <style>
